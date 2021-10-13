22572d7aa3d0f0742a47d95494eea7f38a49ace4
Zairaxx <brandontsegai@gmail.com>
2021-10-13 17:13:11 +0200

    uppdaterad med lösning för uppgift 1a och 1b

--- exempellosning.js
+++ exempellosning.js
@@ -1,20 +1,110 @@
 //Uppgift 1
 
 let addTodoButton = document.querySelector("#addTodo");
-let textInput = document.querySelector("input[name='todo']");
+
+// let textInput = document.querySelector("input[name='todo']");
+let textInput = document.querySelector("#todo");
 
 let todoList = document.querySelector("#todoList");
 
 //När vi klickar på KNAPP, skriv ut värdet av INPUT till LIST i ett LI-element
 // Extra: Skapa logik för att inte lägga in samma värde två gånger.
+
+// Lägg in varje todo vi skapar i en array. Ta arrayens längd för att kolla antal. Loopa igenom för att se existerande todos.
+let myTodos = [];
+
+// addTodoButton.addEventListener("click", () => {
+
+//     if(textInput.value === ""){
+//         alert("Input is empty. Please enter a task.")
+//     } else if (myTodos.length>=5){
+//         alert("You have too many todos! Finish a task before entering a new one.")
+//     } else if (myTodos.includes(textInput.value.toLowerCase())){
+//         alert("Todo already exists!")
+//     }
+//      else {
+//         myTodos.push(textInput.value.toLowerCase());
+//         let listItem = document.createElement("li"); // Skapa li-tag
+//         listItem.innerText = textInput.value; //Ge värdet av användarens inmatning
+//         todoList.appendChild(listItem); // Lägg till i DOM:en
+//         textInput.value = "";
+//         console.log(myTodos);
+//     }
+// })
+
 addTodoButton.addEventListener("click", () => {
-    if(textInput.value !== ""){
+
+    if(textInput.value === ""){
+        alert("Input is empty. Please enter a task.")
+    } else if (myTodos.length>=5){
+        alert("You have too many todos! Finish a task before entering a new one.")
+    } else if (myTodos.includes(textInput.value.toLowerCase())){
+        alert("Todo already exists!")
+    }
+     else {
+        myTodos.push(textInput.value.toLowerCase());
         let listItem = document.createElement("li"); // Skapa li-tag
-        listItem.innerText = textInput.value; //Ge värdet av användarens inmatning
+        let listText = document.createElement("span");
+        let listItemButton = document.createElement("button");
+
+        listItemButton.addEventListener("click", (e) => {
+            let listItemValue = e.target.previousElementSibling.innerText;
+            console.log(listItemValue);
+            myTodos.forEach((todo,index) => {
+                if(todo === listItemValue.toLowerCase()){
+                    myTodos.splice(index,1,"done");
+                }
+            })
+            let liParent = e.target.parentElement;
+            liParent.remove();
+        })
+        listText.innerText = textInput.value;
+        listItemButton.innerText = "Delete"
+        
+        listItem.appendChild(listText);
+        listItem.appendChild(listItemButton);
+         //Ge värdet av användarens inmatning
         todoList.appendChild(listItem); // Lägg till i DOM:en
         textInput.value = "";
+        console.log(myTodos);
     }
 })
+
+// addTodoButton.addEventListener("click", () => {
+
+//     if(textInput.value === ""){
+//         alert("Input is empty. Please enter a task.")
+//     } else if (myTodos.length>=5){
+//         alert("You have too many todos! Finish a task before entering a new one.")
+//     } else if (myTodos.includes(textInput.value.toLowerCase())){
+//         alert("Todo already exists!")
+//     }
+//      else {
+//         myTodos.push(textInput.value.toLowerCase());
+//         let listItem = document.createElement("li"); // Skapa li-tag
+//         let listText = document.createElement("span");
+
+//         let listItemButton = document.createElement("button");
+//         listItemButton.addEventListener("click", (e) => {
+
+//             let liParent = e.target.parentElement;
+//             liParent.previousElementSibling.appendChild(listItemButton);
+//             liParent.remove();
+//         })
+//         listText.innerText = textInput.value;
+//         listItemButton.innerText = "Delete"
+//         listItem.appendChild(listText);
+//         listItem.appendChild(listItemButton);
+//          //Ge värdet av användarens inmatning
+//         todoList.appendChild(listItem); // Lägg till i DOM:en
+//         textInput.value = "";
+//         if(listItem.previousElementSibling !== null){
+//             let prevElement = listItem.previousElementSibling;
+//             prevElement.lastChild.remove();
+//         }
+
+//     }
+// })
 
 //Uppgift 2
 
